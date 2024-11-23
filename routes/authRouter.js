c612e9fac005cab95a94102168cbe7a11a7e2dbd
juancarlosgt <juancarlosgonzaleztorres4@gmail.com>
2024-11-23 12:15:03 -0500

    response userdata at login

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -40,7 +40,7 @@ router.post('/login', async (req, res) => {
       { expiresIn: '1h' }
     );
 
-    res.json({ token });
+    res.json({ token ,user});
   } catch (error) {
     res.status(500).json({ message: 'Error del servidor.', error });
   }
