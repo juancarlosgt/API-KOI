002c301366931aa46abdc6d193d1d64b241ac472
juancarlosgt <juancarlosgonzaleztorres4@gmail.com>
2025-04-18 19:38:41 -0500

    ojala_si

--- controllers/camisetaController.js
+++ controllers/camisetaController.js
@@ -3,6 +3,8 @@
 const Camiseta = require('../models/camiseta');
 const Color = require('../models/color')
 const Imagen = require('../models/imagen')
+const TipoCamisa = require('../models/tipoCamisa')
+const Talla = require('../models/talla')
 const get = async (req, res) => {
   try {
     const camisetas = await Camiseta.findAll();
@@ -29,36 +31,36 @@ const getById = async (req, res) => {
 };
 
 const create = async (req, res) => {
-    try {
-      console.log(req.body); 
-      const { nombre,lanzamiento,oferta,subColeccionId} = req.body;
-      if (!nombre || !subColeccionId) {
-        res.status(400).json({ mensaje: 'Faltan datos obligatorios' });
-        return;
-      }
-      const camiseta = await Camiseta.create({ nombre,lanzamiento,oferta,subColeccionId });
-      res.json(camiseta);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ mensaje: 'Error al crear camiseta' });
+  try {
+    console.log(req.body);
+    const { nombre, lanzamiento, oferta, subColeccionId } = req.body;
+    if (!nombre || !subColeccionId) {
+      res.status(400).json({ mensaje: 'Faltan datos obligatorios' });
+      return;
     }
-  };
+    const camiseta = await Camiseta.create({ nombre, lanzamiento, oferta, subColeccionId });
+    res.json(camiseta);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al crear camiseta' });
+  }
+};
 const update = async (req, res) => {
   try {
     const id = req.params.id;
-    const { nombre,lanzamiento,oferta,subColeccionId} = req.body;
+    const { nombre, lanzamiento, oferta, subColeccionId } = req.body;
     const camiseta = await Camiseta.findByPk(id);
     if (!camiseta) {
       res.status(404).json({ mensaje: 'Camiseta no encontrada' });
-    } else if(!nombre){
-        res.status(400).json({ mensaje: 'Faltan datos obligatorios' });
-        return;
+    } else if (!nombre) {
+      res.status(400).json({ mensaje: 'Faltan datos obligatorios' });
+      return;
     }
-    else{
-      camiseta.nombre = nombre;      
-      camiseta.lanzamiento = lanzamiento ;
+    else {
+      camiseta.nombre = nombre;
+      camiseta.lanzamiento = lanzamiento;
       camiseta.oferta = oferta;
-      camiseta.subColeccionId = subColeccionId;  
+      camiseta.subColeccionId = subColeccionId;
       await camiseta.save();
       res.json(camiseta);
     }
@@ -90,7 +92,7 @@ const getLanzamiento = async (req, res) => {
     const camisetas = await Camiseta.findAll({
       where: { lanzamiento: true }
     });
-    return camisetas?res.json(camisetas): [];
+    return camisetas ? res.json(camisetas) : [];
   } catch (error) {
     return res.status(500).json({ message: 'Error al obtener camisetas en lanzamiento', error: error.message });
   }
@@ -100,30 +102,30 @@ const getOferta = async (req, res) => {
     const camisetas = await Camiseta.findAll({
       where: { oferta: true }
     });
-    return camisetas?res.json(camisetas): [];
+    return camisetas ? res.json(camisetas) : [];
   } catch (error) {
     return res.status(500).json({ message: 'Error al obtener camisetas en lanzamiento', error: error.message });
   }
 }
 
-const getColor = async(req,res) =>{
+const getColor = async (req, res) => {
   const { camisetaId } = req.params;
   const camiseta = await Camiseta.findByPk(camisetaId, {
     include: Color
   });
   res.json(camiseta.Colors); // Asumiendo que la relación se llama Colores
 }
-const addColor = async(req,res) =>{
+const addColor = async (req, res) => {
   const { camisetaId, colorId } = req.params;
   const camiseta = await Camiseta.findByPk(camisetaId);
   await camiseta.addColor(colorId); // Método generado
-  res.status(200).json({message:'Camiseta asociada al color'});
+  res.status(200).json({ message: 'Camiseta asociada al color' });
 }
-const deleteColor = async(req,res) =>{
+const deleteColor = async (req, res) => {
   const { camisetaId, colorId } = req.params;
   const camiseta = await Camiseta.findByPk(camisetaId);
   await camiseta.removeColor(colorId); // Método generado
-  res.status(200).json({message:'Camiseta desasociada del color'});
+  res.status(200).json({ message: 'Camiseta desasociada del color' });
 }
 const obtenerImagenesDeCamiseta = async (req, res) => {
   const { camisetaId } = req.params; // ID de la camiseta desde los parámetros de la solicitud
@@ -178,18 +180,144 @@ const eliminarImagen = async (req, res) => {
   }
 };
 
+
+
+// const getTipo = async(req,res) =>{
+//   const { camisetaId } = req.params;
+//   const camiseta = await Camiseta.findByPk(camisetaId, {
+//     include: TipoCamisa
+//   });
+//   res.json(camiseta.TipoCamisas); // Asumiendo que la relación se llama Colores
+// }
+// const addTipo = async(req,res) =>{
+//   const { camisetaId, tipoId } = req.params;
+//   const camiseta = await Camiseta.findByPk(camisetaId);
+//   await camiseta.addTipoCamisa(tipoId); // Método generado
+//   res.status(200).json({message:'Producto asociada al Tipo'});
+// }
+// const deleteTipo = async(req,res) =>{
+//   const { camisetaId, tipoId } = req.params;
+//   const camiseta = await Camiseta.findByPk(camisetaId);
+//   await camiseta.removeTipoCamisa(tipoId); // Método generado
+//   res.status(200).json({message:'Producto desasociado del tipo'});
+// }
+const getTipo = async (req, res) => {
+  try {
+    const { camisetaId } = req.params;
+    const camiseta = await Camiseta.findByPk(camisetaId, {
+      include: TipoCamisa
+    });
+    if (!camiseta) {
+      return res.status(404).json({ message: 'Producto no encontrada' });
+    }
+    res.json(camiseta.TipoCamisas); 
+  } catch (error) {
+    console.error('Error al obtener tipos:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
+
+const addTipo = async (req, res) => {
+
+  try {
+    const { camisetaId, tipoId } = req.params;
+    const camiseta = await Camiseta.findByPk(camisetaId);
+    if (!camiseta) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+    await camiseta.addTipoCamisa(tipoId); 
+    res.status(200).json({ message: 'Producto asociado al Tipo' });
+  } catch (error) {
+    console.error('Error al agregar tipo:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
+
+
+const deleteTipo = async (req, res) => {
+  try {
+    const { camisetaId, tipoId } = req.params;
+    const camiseta = await Camiseta.findByPk(camisetaId);
+    if (!camiseta) {
+      return res.status(404).json({ message: 'Camiseta no encontrada' });
+    }
+    await camiseta.removeTipoCamisa(tipoId);
+    res.status(200).json({ message: 'Producto desasociado del tipo' });
+  } catch (error) {
+    console.error('Error al eliminar tipo:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+
+  }
+
+}
+
+const getTalla = async (req, res) => {
+  try {
+    const { camisetaId } = req.params;
+    const camiseta = await Camiseta.findByPk(camisetaId, {
+      include: Talla
+    });
+    if (!camiseta) {
+      return res.status(404).json({ message: 'Producto no encontrada' });
+    }
+    res.json(camiseta.Tallas); 
+  } catch (error) {
+    console.error('Error al obtener tallas:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
+
+const addTalla = async (req, res) => {
+
+  try {
+    const { camisetaId, tallaId } = req.params;
+    const camiseta = await Camiseta.findByPk(camisetaId);
+    if (!camiseta) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+    await camiseta.addTalla(tallaId); 
+    res.status(200).json({ message: 'Producto asociado a la Talla' });
+  } catch (error) {
+    console.error('Error al agregar talla:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
+
+
+const deleteTalla = async (req, res) => {
+  try {
+    const { camisetaId, tallaId } = req.params;
+    const camiseta = await Camiseta.findByPk(camisetaId);
+    if (!camiseta) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
+    }
+    await camiseta.removeTalla(tallaId);
+    res.status(200).json({ message: 'Producto desasociado de la talla' });
+  } catch (error) {
+    console.error('Error al eliminar talla:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+
+  }
+
+}
 module.exports = {
- get,
- getById,
- create,
- update,
- destroy,
- getLanzamiento,
- getOferta,
- getColor,
- addColor,
- deleteColor,
- agregarImagenACamiseta,
- eliminarImagen,
- obtenerImagenesDeCamiseta
+  get,
+  getById,
+  create,
+  update,
+  destroy,
+  getLanzamiento,
+  getOferta,
+  getColor,
+  addColor,
+  deleteColor,
+  agregarImagenACamiseta,
+  eliminarImagen,
+  obtenerImagenesDeCamiseta,
+  getTipo,
+  addTipo,
+  deleteTipo,
+  getTalla,
+  addTalla,
+  deleteTalla
 };
